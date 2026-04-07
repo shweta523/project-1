b10e3037d835216bd1318fd77bf0ae4c99b6c51c
shweta523 <shweta@qmsmodeltown.in>
2026-04-07 13:28:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
